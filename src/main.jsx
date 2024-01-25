eec94146a54358eb1cb5fe3f3194c04401fae04c
GUNADEVAN D <150560099+gunadevandesing@users.noreply.github.com>
2024-01-25 07:26:58 +0000

    initial set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const themeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("#fff", "#000")(props),
+      color: mode("#000", "whiteAlpha.900")(props),
+    },
+  }),
+};
+
+const theme = extendTheme({ config: themeConfig, styles });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Router>
   </React.StrictMode>,
-)
+);
